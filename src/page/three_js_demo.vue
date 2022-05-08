02cf054ace4d202fe428972b76c40871fa957bf2
r1ader <r1ader@qq.com>
2022-05-09 00:47:03 +0800

    test(threejs): threejs demo

--- src/page/three_js_demo.vue
+++ src/page/three_js_demo.vue
@@ -1,9 +1,41 @@
 <script setup>
+import * as THREE from 'three'
+import { onMounted, ref } from "vue";
 
+const mainDiv = ref(0)
+onMounted(() => {
+
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  mainDiv.value.appendChild(renderer.domElement)
+
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+  }
+
+  animate();
+})
 </script>
 
 <template>
-  <div>threejs</div>
+  <div ref="mainDiv">
+
+  </div>
 </template>
 
 
